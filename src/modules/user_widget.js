70a5211d4d8dd5b0c60cdd2c5f0cddfd0c357b12
Brittany Starr <lunarunar@gmail.com>
2017-09-19 10:19:10 -0700

    basic add-only functionality

--- src/modules/user_widget.js
+++ src/modules/user_widget.js
@@ -1,14 +1,34 @@
 import $ from 'jquery';
 
 class UserWidget {
-  constructor(id) {
+  constructor(selector) {
+    this.$widgetItem= selector;
     this.state = {};
-    this.attachListeners();
+    this.attachInitialListeners();
   }
   
-  attachListeners() {
+  attachInitialListeners() {
     $(document).on('newMatches', (e, newMatches) => {
       this.createItem(newMatches);
+    });
+  }
+  
+  attachItemListeners(selector) {
+    //once results exist, add listeners to them
+    $(selector).click(e => this.addToComment(e));
+  }
+  
+  addToComment(event) {
+    let item = $(event.target);
+    let user;
+    if (item[0].className && item[0].className !== 'result-item') {
+      item = $(item).parent('.result-item')[0];
+    }
+    //get user's name
+    user = $(item).children('.result-user')[0].textContent;
+    //append to the comment box
+    $('.comment-block').val((i, value) => {
+        return `${value} ${user} `;
     });
   }
   
@@ -20,11 +40,13 @@ class UserWidget {
     users.map((user, i) => {
       const item = `<div class="result-item" data-result-id="${i}">
                       <img src="${user.avatar_url}" alt="user profile pictures" class="result-img"/>    
-                      <p>${user.name}</p>
+                      <p class="result-user">${user.name}</p>
                     </div>`;
       $('#widget').append(item);
-      $('#widget').show();
     });
+    
+    this.attachItemListeners('.result-item');
+    $('#widget').show();
   }
   
   setState(addToState) {
